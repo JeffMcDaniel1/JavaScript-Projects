c85e885f5ec92994672a806a67eac99f331fa088
Jeffrey McDaniel <47953478+JeffMcDaniel1@users.noreply.github.com>
2020-04-29 17:26:53 -0500

    123
    123

---
+++ js/Basic_JavaScript_1.js
@@ -0,0 +1,21 @@
+var A = "This is a string";
+var A = A.fontcolor("orange");
+document.write(A);
+var B = new String("Television");
+window.alert(B);
+var C = new String("This is how to display \\ in your code");
+var C = C.fontcolor("green");
+document.write(C);
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+var str1 = new String("Hello");
+var str1 = str1.fontcolor("yellow");
+var str2 = new String ("world!");
+var str2 = str2.fontcolor("blue");
+var res = str1.concat(str2);
+document.write(res);
+var Family = "The McDaniels", Mother="Deb", Dad="Roger", Son="John", Daughter = "Jammie";
+var Family = Family.fontcolor("red");
+document.write(Dad);
+document.write(3+5);
