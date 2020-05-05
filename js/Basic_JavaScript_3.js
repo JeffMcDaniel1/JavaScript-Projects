7cecf3ba724a8066a72c3f8e69c865afd65a5b27
Jeffrey McDaniel <47953478+JeffMcDaniel1@users.noreply.github.com>
2020-05-05 18:30:38 -0500

    123

--- js/Basic_JavaScript_3.js
+++ js/Basic_JavaScript_3.js
@@ -7,5 +7,16 @@ function myFunction() {
 function subtraction_Function(){
     var Subtraction= 5-2;
     document.getElementById("Subtraction").innerHTML = Subtraction;
+window.alert(Math.random() * 100);
+var x = 7;
+x++;
+document.write(x);
+}
+
+function modulus_Operator(){
+    var simple_Math = 25 % 6;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
 
 }
+
+
