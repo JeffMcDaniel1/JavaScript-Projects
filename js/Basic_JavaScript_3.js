2f72000de355108e19746826cbd7a019c1ab41ae
Jeffrey McDaniel <47953478+JeffMcDaniel1@users.noreply.github.com>
2020-05-05 18:22:06 -0500

    1232
    123

---
+++ js/Basic_JavaScript_3.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var a = 2;
+    var b = 6;
+    var c = a+b;
+    document.getElementById("Addition").innerHTML = c;
+}
+function subtraction_Function(){
+    var Subtraction= 5-2;
+    document.getElementById("Subtraction").innerHTML = Subtraction;
+
+}
